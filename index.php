ab17f01e5032ab502a0090f4f51315ec5ebefa4a
Sam Malefyt <sdwm33@gmail.com>
2017-05-05 22:00:01 -0500

    updated heroku files

--- index.php
+++ index.php
@@ -1,2 +1,2 @@
-<?php include_once("/app/public/home.html"); ?>
-<?php include_once("/app/public/survey.html"); ?>
+<?php include_once("./app/public/home.html"); ?>
+<?php include_once("./app/public/survey.html"); ?>
